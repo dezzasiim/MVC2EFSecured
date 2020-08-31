0f9d3ae0a2836aca3d9a8753998301c6ed891baa
Derek Sym <derek_sym@outlook.com>
2020-08-31 12:49:51 -0500

    Initial Commit

---
+++ MVC2EFSecured.UI.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC2EFSecured.UI.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
