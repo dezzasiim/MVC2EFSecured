feddd92e5e1435e4a3e9c1a5458ae19dfe12ce2e
Derek Sym <derek_sym@outlook.com>
2020-08-31 13:17:12 -0500

    Installed IdentitySamples

--- MVC2EFSecured.UI.MVC/Startup.cs
+++ MVC2EFSecured.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(MVC2EFSecured.UI.MVC.Startup))]
 namespace MVC2EFSecured.UI.MVC
 {
     public partial class Startup
